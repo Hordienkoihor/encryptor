64e0c36c0eb8ddf3a2ee48c55ea232da4f9c90f8
Hordienkoihor <hordienkoihor05@gmail.com>
2026-01-05 20:12:34 +0200

    cors for front

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ async function bootstrap() {
   const PORT = process.env.PORT ?? 3000
 
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    method: 'POST',
+    credentials: 'true',
+  })
+
   await app.listen(PORT, () => {
     console.log(`Server started on port ${PORT}`);
   });
